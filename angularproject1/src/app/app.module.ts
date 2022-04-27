bfbaf4b2a9af6ceea02b3bbf8c72164fd2a7f025
kyle <kyle.a.watkins1@gmail.com>
2022-04-27 22:03:12 +1000

    added personal details component and navigation

--- angularproject1/src/app/app.module.ts
+++ angularproject1/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonalDetailsComponent
+    PersonalDetailsComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
