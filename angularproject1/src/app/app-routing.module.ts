593a953b6c90a6997c1032b16f1b0e6d600f32cf
Kyle Watkins <kyle.a.watkins1@gmail.com>
2022-05-07 02:55:46 +1000

    Generated routes for next button in personal details form

--- angularproject1/src/app/app-routing.module.ts
+++ angularproject1/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { Router, RouterModule, Routes } from '@angular/router';
 import { PersonalDetailsComponent } from './personal-details/personal-details.component';
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
+import { EmploymentComponent } from './employment/employment.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'personal', component: PersonalDetailsComponent }
+  { path: 'personal', component: PersonalDetailsComponent },
+  { path: 'employment', component: EmploymentComponent }
   ]
 
 
